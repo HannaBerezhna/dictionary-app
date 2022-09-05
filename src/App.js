0b48e1fcd255c7f95e8e703eafbb27d0fa65ec4f
HannaBerezhna <108458530+HannaBerezhna@users.noreply.github.com>
2022-09-05 21:23:59 +0100

    added word meanings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React from "react";
 import "./App.css";
-import Dictionary from "./Dictionary";
 
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
-let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(keyword);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateKeyword(event) {
@@ -25,6 +33,7 @@ let [keyword, setKeyword] = useState("");
         />
         <input type="submit" value="Search" />
       </form>
+      <Results results={results}/>
     </div>
   );
 }
