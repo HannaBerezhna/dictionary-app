8777a7a3ac9be5b144e80f7b6f132f42bae8f556
HannaBerezhna <108458530+HannaBerezhna@users.noreply.github.com>
2022-09-12 21:26:04 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -9,8 +9,35 @@ function App() {
       <div className="cointainer">
         <header className="App-header">
           <h1>D i c t i o n a r y</h1>
-          <Dictionary />
         </header>
+        <Dictionary />
+        <footer>
+          This project was coded by Hanna Berezhna
+          <br />
+          <div className="d-flex justify-content-center">
+            <a
+              href="https://www.instagram.com/gannaberezhna/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/hanna-berezhna-88277a234/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a
+              href="https://github.com/HannaBerezhna/dictionary-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-github-square"></i>
+            </a>
+          </div>
+        </footer>
       </div>
     </div>
   );
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer">
-        Listen
+        <i className="fa-regular fa-circle-play"></i>
       </a>
       <span className="text">{props.phonetic.text}</span>
     </div>
